099cab5d651eff7489a4808f5d46a54fb43b6a3d
ReDLiX123 <andrej.krekhov@yandex.ru>
2025-11-07 16:00:00 +0800

    Add Comments

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ app = FastAPI()
 
 db = []
 
+
 def next_id():
     if not db:
         return 1
     return db[-1]["id"] + 1
 
 
-
+# GET-запрос на получение списка книг
 @app.get("/books", response_model=list[Book], status_code=status.HTTP_200_OK, description='Получить список книг из базы данных')
 def get_books(
     skip: int = Query(0, description='Кол-во книнг, которые нужно пропустить', ge=0),
@@ -21,6 +22,8 @@ def get_books(
 ):
     return db[skip: skip + limit]
 
+
+# GET-запрос на получение одной книги по ID
 @app.get("/books/{book_id}", response_model=Book, status_code=status.HTTP_200_OK, description='Получить одну книгу по ее ID из базы данных')
 def read_book(book_id: int):
     for book in db:
@@ -28,6 +31,9 @@ def read_book(book_id: int):
             return book
     raise HTTPException(status_code=404, detail='Книга не найдена')
 
+
+
+# POST-запрос на создание книге в базе данных
 @app.post("/books", response_model=Book, status_code=status.HTTP_201_CREATED, description='Создать книгу')
 def create_book(book: BookCreate):
     book_data = book.model_dump()
@@ -40,6 +46,8 @@ def create_book(book: BookCreate):
     return book_data
 
 
+
+# PUT-запрос на обновление параметров книги по ее ID
 @app.put("/books/{book_id}", response_model=Book, status_code=status.HTTP_200_OK, description='Обновление параметров книги по ее ID')
 def update_book(book_id: int, book_update: BookUpdate):
 
@@ -51,6 +59,8 @@ def update_book(book_id: int, book_update: BookUpdate):
     raise HTTPException(status_code=400, detail='Книги с таким ID не найдено')
 
 
+
+# DELETE-запрос на удаление книги из базы данных по ее ID
 @app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT, description='Удаление книги из базы данных')
 def delete_book(book_id: int):
     for book in db:
